ddca76826203867a328bafed8a31a8a2c0692ec0
bishoyabdmariam <bishoyabdo236@gmail.com>
2025-04-01 18:16:14 +0000

    Deploy Flutter Web Build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1094529921"
+    serviceWorkerVersion: "3922985183"
   }
 });
